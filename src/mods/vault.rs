f7a277c0ec01cdd330b2bb4f9babafa32abadcf8
Zach <22am014@sctce.ac.in>
2026-03-03 19:50:58 +0530

    feat: add argon2 key derivation and aes-gcm encrypt/decrypt

--- src/mods/vault.rs
+++ src/mods/vault.rs
@@ -1,4 +1,11 @@
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use argon2::Argon2;
+use base64::{engine::general_purpose::STANDARD as BASE64, Engine as _};
 use serde::{Deserialize, Serialize};
+use zeroize::Zeroize;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct VaultEntry {
@@ -22,4 +29,30 @@ struct VaultFile {
     salt: String,
     nonce: String,
     ciphertext: String,
+}
+
+fn derive_key(master_password: &str, salt: &[u8]) -> [u8; 32] {
+    let mut key = [0u8; 32];
+    Argon2::default()
+        .hash_password_into(master_password.as_bytes(), salt, &mut key)
+        .expect("argon2 key derivation failed");
+    key
+}
+
+fn encrypt(key_bytes: &[u8; 32], plaintext: &[u8]) -> (Vec<u8>, Vec<u8>) {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key_bytes));
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let ciphertext = cipher
+        .encrypt(&nonce, plaintext)
+        .expect("encryption failed");
+    (nonce.to_vec(), ciphertext)
+}
+
+fn decrypt(
+    key_bytes: &[u8; 32],
+    nonce: &[u8],
+    ciphertext: &[u8],
+) -> Result<Vec<u8>, aes_gcm::Error> {
+    let cipher = Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key_bytes));
+    cipher.decrypt(Nonce::from_slice(nonce), ciphertext)
 }
